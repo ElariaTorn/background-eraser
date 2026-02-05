8a0556c6c3e3cf9e6c8496bac3e94618db47a0c3
Elari <elariatorn@gmail.com>
2026-02-05 23:35:46 +0300

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,15 +5,6 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-})
-
-
-export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
