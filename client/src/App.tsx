72c7a17d3f4192926c2436b1cd1f8e14414af9dc
Elari <elariatorn@gmail.com>
2026-02-07 23:40:01 +0300

    fix: updating main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,12 @@
 import { Switch, Route } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/Home";
 import Create from "@/pages/Create";
 import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Home} />
       <Route path="/create" component={Create} />
       <Route component={NotFound} />
     </Switch>
